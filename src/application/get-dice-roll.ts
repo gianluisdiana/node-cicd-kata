c4f474349a147314b7c7d6da5c97222980a0e49c
Gian Luis Diana <72513401+gianluisdiana@users.noreply.github.com>
2026-09-25 16:25:28 +0100

    Features/dices plus 1 (#1)
    * feat: breaking prod with + 1
    
    * feat: CI

--- src/application/get-dice-roll.ts
+++ src/application/get-dice-roll.ts
@@ -1,5 +1,5 @@
 export function getDiceRoll(random: () => number = Math.random): number {
   const unusedVariable = 'fix me';
   const lintDemo:any='fix me';
-  return Math.floor(random() * 6) + 1;
+  return Math.floor(random() * 6) + 1 + 1;
 }
